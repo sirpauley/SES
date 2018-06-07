a3e63cc6a4983e96139b901e30c5fda811c4b33c
sirpauley <sirpauley@gmail.com>
2018-06-07 16:18:35 +0200

    admin_user update

--- setup/admin_user.sql
+++ setup/admin_user.sql
@@ -4,11 +4,13 @@ user: admin
 password: admin
 */
 
+
 INSERT INTO `joblevel` (`ID`, `level`, `description`) VALUES
 (1, 1, 'super admin');
 
-INSERT INTO `employee` (`ID`, `user_id`, `fullname`, `surname`, `position_id`, `employed_date`, `birthday`, `tell`, `email`, `ACTIVE`) VALUES
-(1, 1, 'Admin', 'Admin', 1, '2018-01-01', '2018-01-01', '0123456789', 'admin@admin.admin', 1);
 
 INSERT INTO `user` (`ID`, `user`, `password`) VALUES
 (1, 'admin', '21232f297a57a5a743894a0e4a801fc3');
+
+INSERT INTO `employee` (`ID`, `user_id`, `fullname`, `surname`, `position_id`, `employed_date`, `birthday`, `tell`, `email`, `ACTIVE`) VALUES
+(1, 1, 'Admin', 'Admin', 1, '2018-01-01', '2018-01-01', '0123456789', 'admin@admin.admin', 1);
